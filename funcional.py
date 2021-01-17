3bb62e037c30ee5ca959e4413fbf9047bf264e59
Carlos de Jesus Gutierrez Loaeza <7153253-GutierrezLoaezaCarlosdeJesus@users.noreply.gitlab.com>
2021-01-16 18:49:51 -0600

    Programación funcional

---
+++ funcional.py
@@ -0,0 +1,13 @@
+# La programación funcional es un paradigma en el que la programación 
+# se basa casi en su totalidad en funciones, entendiendo el concepto de 
+# función según su definición matemática, y no como los simples subprogramas 
+# de los lenguajes imperativos que hemos visto hasta ahora.
+
+# En los lenguajes funcionales puros un programa consiste exclusivamente en 
+# la aplicación de distintas funciones a un valor de entrada para obtener un 
+# valor de salida.
+# 
+# Python, sin ser un lenguaje puramente funcional incluye varias características 
+# tomadas de los lenguajes funcionales como son las funciones de orden superior 
+# o las funciones lambda (funciones anónimas)
+
